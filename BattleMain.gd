df7d813fb21c936fb7dde605ff5a60623dce9bdd
John Nicholson <78463373+nicholso24@users.noreply.github.com>
2024-09-25 14:04:45 -0700

    removed camera stuff

--- BattleMain.gd
+++ BattleMain.gd
@@ -1,32 +1,17 @@
 extends Node3D
 
-const CAMERA = preload("res://CameraEnum.gd")
 var crab_scene = load("res://Crabball.tscn")
 var score = 0
 var crab_pot = []
 
-var camera_angle = 0
-var sens_horizontal = 0.2
-
-var current_cam = CAMERA.STATIC
-@onready var cameras = [$STATIC, $Player/FIRST,$Player/ThirdPivot/SpringArm3D/THIRD,$Player/CROW,$CAMERA]
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Player.position = $PlayerSpawn.position
-	$STATIC.current = true
-
-func _input(event):
-	if current_cam == CAMERA.CAMERA:
-		if event is InputEventMouseMotion:
-			$CAMERA.rotate_y(deg_to_rad(-event.relative.x*sens_horizontal))
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
-	if Input.is_action_just_pressed("Change_Camera"):
-		change_camera()
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_enemy()
@@ -46,7 +31,6 @@ func _on_player_death() -> void:
 	for crabball in crab_pot:
 		crabball.queue_free()
 	$EndGameTimer.start()
-	$CanvasLayer/HUD.end_game()
 
 func game_over():
 	get_tree().quit()
@@ -54,27 +38,12 @@ func game_over():
 
 func _on_player_damaged():
 	$Player.take_damage()
-	$CanvasLayer/HUD.update_health($Player.health)
 
 
 func _on_score_timer_timeout() -> void:
 	score += 1
-	$CanvasLayer/HUD.update_score(score)
 
 
 func _on_end_game_timer_timeout() -> void:
 	game_over() 
-
-func change_camera():
-	if(current_cam == CAMERA.CAMERA):
-		current_cam = CAMERA.STATIC
-		$Player.current_cam = CAMERA.STATIC
-	else:
-		current_cam += 1
-		$Player.current_cam += 1
-	if current_cam == CAMERA.FIRST || current_cam == CAMERA.THIRD:
-		$Player.cam_can_move = true
-	else:
-		$Player.cam_can_move = false
-	cameras[current_cam].current = true
 	
--- Player.gd
+++ Player.gd
@@ -10,7 +10,6 @@ var is_third = false
 var sens_horizontal = 0.2
 var sens_vertical = 0.2
 
-var current_cam = CAMERA.STATIC
 var health = 9
 var iframes =  false
 
@@ -19,157 +18,14 @@ var cam_can_move = false
 signal death
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass
 	
-func _input(event):
-	if cam_can_move:
-		if event is InputEventMouseMotion:
-			rotate_y(deg_to_rad(-event.relative.x*sens_horizontal))
-			$ThirdPivot.rotate_x(deg_to_rad(-event.relative.y*sens_vertical))
-			$FIRST.rotate_x(deg_to_rad(-event.relative.y*sens_vertical))
-		
-func _physics_process(delta: float) -> void:
-	match current_cam:
-		CAMERA.STATIC:
-			static_movement(delta)
-		CAMERA.FIRST:
-			first_movement(delta)
-		CAMERA.THIRD:
-			third_movement(delta)
-		CAMERA.CROW:
-			crow_movement(delta)
-		CAMERA.CAMERA:
-			camera_movement(delta)
-			
 func _process(delta: float) -> void:
 	if(velocity.x == 0 && velocity.z == 0 && is_on_floor()):
 		$Helper.idle()
 	elif(is_on_floor()):
 		$Helper.run()
-		
-func static_movement(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$Helper.jump()
-		velocity.y = JUMP_VELOCITY
 	
-	if Input.is_action_pressed("Rotate_Right"):
-		rotate_y(-0.1)
-	if Input.is_action_pressed("Rotate_Left"):
-		rotate_y(0.1)
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Foward", "Move_Back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	move_and_slide()
-
-func first_movement(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$Helper.jump()
-		velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Foward", "Move_Back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	move_and_slide()
-
-func third_movement(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$Helper.jump()
-		velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Foward", "Move_Back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	move_and_slide()
-
-func crow_movement(delta):
-		# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$Helper.jump()
-		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_pressed("Rotate_Right"):
-		rotate_y(-0.1)
-	if Input.is_action_pressed("Rotate_Left"):
-		rotate_y(0.1)
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Foward", "Move_Back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	move_and_slide()
-	
-func camera_movement(delta):
-	
-		# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$Helper.jump()
-		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_pressed("Rotate_Right"):
-		rotate_y(-0.1)
-	if Input.is_action_pressed("Rotate_Left"):
-		rotate_y(0.1)
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Foward", "Move_Back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	move_and_slide()
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	take_damage()
 
